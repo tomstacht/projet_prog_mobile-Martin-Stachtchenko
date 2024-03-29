b0fec60d3ef663c4493bec90a66ec18b7ef16776
anatolemartin <anatole.martin@edu.ece.fr>
2024-03-29 17:55:47 +0100

    ajout itempage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,6 +3,7 @@ import 'horizontal_items_grid.dart';
 import 'comic.dart';
 import 'section.dart';
 import 'serie.dart';
+import 'item_card.dart'; // Importez la classe ItemCard
 
 class HomePage extends StatelessWidget {
   final List<Serie> mockSeries = List.generate(10, (index) => Serie.mock());
@@ -12,14 +13,56 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: const Color(0xFF15232E), // Set the background color
+      color: const Color(0xFF15232E), // Définissez la couleur d'arrière-plan
       child: SingleChildScrollView(
         child: Column(
           children: [
             _buildHeader(),
-            Section(title: 'Séries populaires', child: HorizontalItemGrid(series: mockSeries)),
-            Section(title: 'Comics populaires', child: HorizontalItemGrid(series: mockComics)),
-            Section(title: 'Films populaires', child: HorizontalItemGrid(series: mockFilms)),
+            Section(
+              title: 'Séries populaires',
+              child: HorizontalItemGrid<Serie>(
+                items: mockSeries,
+                itemBuilder: (serie) => ItemCard<Serie>(
+                  item: serie,
+                  imageUrlField: serie.imageUrl,
+                  titleField: serie.titre,
+                  category: 'Série',
+                  onTap: (item) {
+                    // Logique à exécuter lors du clic sur la carte de série
+                  },
+                ),
+              ),
+            ),
+            Section(
+              title: 'Comics populaires',
+              child: HorizontalItemGrid<Comic>(
+                items: mockComics,
+                itemBuilder: (comic) => ItemCard<Comic>(
+                  item: comic,
+                  imageUrlField: comic.imageUrl,
+                  titleField: comic.titre,
+                  category: 'Comic',
+                  onTap: (item) {
+                    // Logique à exécuter lors du clic sur la carte de comic
+                  },
+                ),
+              ),
+            ),
+            Section(
+              title: 'Films populaires',
+              child: HorizontalItemGrid<Comic>(
+                items: mockFilms,
+                itemBuilder: (film) => ItemCard<Comic>(
+                  item: film,
+                  imageUrlField: film.imageUrl,
+                  titleField: film.titre,
+                  category: 'Film',
+                  onTap: (item) {
+                    // Logique à exécuter lors du clic sur la carte de film
+                  },
+                ),
+              ),
+            ),
           ],
         ),
       ),
