a320106d7368db4e06df564f8145f0a8ab58ce34
anatolemartin <anatole.martin@edu.ece.fr>
2024-03-30 11:15:50 +0100

    ajout film

--- lib/films_page.dart
+++ lib/films_page.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
-import 'comic_card.dart';
-import 'package:projet_prog_mobile/comic.dart'; // Adjust the path to where your Comic class is defined
+import 'film_card.dart';
+import 'film.dart'; // Adjust the path to where your Comic class is defined
+import 'film_detail_page.dart'; // Adjust the path to where your Comic class is defined
 
 
 class FilmsPage extends StatelessWidget {
-  final List<Comic> mockComics = List.generate(10, (index) => Comic.mock());
+  final List<Film> mockFilms = List.generate(10, (index) => Film.mock());
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: const Color(0xFF15232E), // Set the background color
+      color: const Color(0xFF15232E),
       child: SingleChildScrollView(
         child: Column(
           children: [
             _buildHeader(),
-            ...mockComics.map((comic) => ComicCard(comic: comic, rank: 1)).toList(),
+            ...mockFilms.map((film) {
+              // Utilisez GestureDetector ou InkWell pour envelopper ComicCard
+              return GestureDetector(
+                onTap: () {
+                  // Navigation vers ComicDetailPage avec le comic cliqué
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => FilmDetailPage(film: film, selectedCategory: 'histoire',)),
+                  );
+                },
+                child: FilmCard(film: film, rank: 1),
+              );
+            }).toList(),
           ],
         ),
       ),
@@ -23,7 +36,7 @@ class FilmsPage extends StatelessWidget {
 
 
   Widget _buildHeader() {
-    return Padding(
+    return const Padding(
       padding: EdgeInsets.only(left: 5, top: 10, bottom: 20),
       child: Text(
         'Films les plus populaires',
