2a7b3c39f34c9d7d6573138a8df6f8982bde0e71
anatolemartin <anatole.martin@edu.ece.fr>
2024-03-31 22:47:46 +0200

    ajout système api

--- lib/comic_card.dart
+++ lib/comic_card.dart
@@ -20,69 +20,73 @@ class ComicCard extends StatelessWidget {
             color: Colors.white.withOpacity(0.1), // Slightly transparent white card
             borderRadius: BorderRadius.circular(8),
           ),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Image.asset(
-                comic.imageUrl,
-                width: 100,
-                height: 150,
-                fit: BoxFit.cover,
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        comic.name,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 16,
-                        ),
-                      ),
+          child: IntrinsicHeight( // Wrap Row with IntrinsicHeight
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Image.network(
+                  comic.imageUrl, // Change Image.asset to Image.network
+                  width: 100,
+                  height: 150,
+                  fit: BoxFit.cover,
+                ),
+                Flexible( // Use Flexible instead of Expanded
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
                         Text(
-                          comic.description ?? 'Description non disponible', // Utilisation de l'opérateur de coalescence nulle
+                          comic.name,
                           style: const TextStyle(
                             fontWeight: FontWeight.bold,
                             color: Colors.white,
                             fontSize: 16,
                           ),
                         ),
-                      Text(
-                        '№ ${comic.issueNumber}',
-                        style: TextStyle(
-                          color: Colors.white.withOpacity(0.7),
-                          fontSize: 14,
+                        Text(
+                          comic.description ?? 'Description non disponible',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                          overflow: TextOverflow.ellipsis, // Prevent overflow
+                          maxLines: 3, // Limit the number of lines
                         ),
-                      ),
-                      Text(
-                        comic.coverDate,
-                        style: TextStyle(
-                          color: Colors.white.withOpacity(0.7),
-                          fontSize: 12,
+                        Text(
+                          '№ ${comic.issueNumber}',
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.7),
+                            fontSize: 14,
+                          ),
                         ),
-                      ),
-                    ],
+                        Text(
+                          comic.coverDate,
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.7),
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-        Positioned( // Add this widget to position the rank
+        Positioned(
           top: 10,
           left: 10,
           child: Container(
             padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
             decoration: BoxDecoration(
-              color: Colors.orange, // Change to the color you want
-              borderRadius: BorderRadius.circular(20), // Rounded edges
+              color: Colors.orange,
+              borderRadius: BorderRadius.circular(20),
             ),
             child: Text(
-              '#$rank', // Show the rank number
+              '#$rank',
               style: const TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
